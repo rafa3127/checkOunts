f25da78cd517301ab02322e2a95d9a90d7728dc5
Rafael Correa <rafaelc3127@gmail.com>
2021-12-23 10:43:30 -0400

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { Plugins,  } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 import { AuthService } from './core/services/auth.service';
+
+const { SplashScreen, StatusBarStyle, StatusBar } = Plugins
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,6 +11,20 @@ import { AuthService } from './core/services/auth.service';
 })
 export class AppComponent {
 
-  constructor() {}
+  constructor(
+    private platform: Platform
+  ) {
+    this.initializeApp()
+  }
 
+
+  initializeApp(){
+    this.platform.ready().then(() =>{
+      SplashScreen.hide()
+      StatusBar.setBackGroundColor({color: '#f48434'})
+      StatusBar.setStyle({
+        style: StatusBarStyle.Light
+      })
+    })
+  }
 }
